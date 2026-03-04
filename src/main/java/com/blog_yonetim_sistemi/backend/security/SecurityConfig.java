b47cac3efeb98fed1ea663510c5929d632c2868a
Tuğçe Özkök <1030521211@erciyes.edu.tr>
2026-03-04 16:45:13 +0300

    add JwtAuthenticationFilter and update security configuration

--- src/main/java/com/blog_yonetim_sistemi/backend/security/SecurityConfig.java
+++ src/main/java/com/blog_yonetim_sistemi/backend/security/SecurityConfig.java
@@ -7,21 +7,31 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    // Login işlemi için
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+
     @Bean
     public AuthenticationManager authenticationManager(
             AuthenticationConfiguration config) throws Exception {
-
         return config.getAuthenticationManager();
     }
 
-    // Security kuralları burada
+    /*
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    } */
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)
             throws Exception {
@@ -31,9 +41,22 @@ public class SecurityConfig {
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(
                                 SessionCreationPolicy.STATELESS))
+
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/**").permitAll()
+
+                        .requestMatchers("/api/auth/**").permitAll()
+
+
+                        .requestMatchers("/api/users/register").permitAll()
+
+
                         .anyRequest().authenticated()
+                )
+
+
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
 
         return http.build();
